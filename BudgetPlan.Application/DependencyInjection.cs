4366e942ed708d6b9a72c8ae6b0887001aad5846
Adam Ludwiczak <adam.ludwiczak@keikostudio.com>
2023-07-30 21:04:41 +0200

    Added fluent validation pipeline

--- BudgetPlan.Application/DependencyInjection.cs
+++ BudgetPlan.Application/DependencyInjection.cs
@@ -1,4 +1,7 @@
 using System.Reflection;
+using BudgetPlan.Application.Common.Behaviours;
+using FluentValidation;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BudgetPlan.Application;
@@ -7,8 +10,14 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
+        // DI
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
         services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
         services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        
+        // Pipelines
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+        
         return services;
     }
 }
