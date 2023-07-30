ed2c3a3fefc56f15585b442933a77e022c4008b6
Adam Ludwiczak <adam.ludwiczak@keikostudio.com>
2023-07-30 21:43:24 +0200

    Update IBudgetPlanDbContext.cs

--- BudgetPlan.Application/Common/Interfaces/IBudgetPlanDbContext.cs
+++ BudgetPlan.Application/Common/Interfaces/IBudgetPlanDbContext.cs
@@ -1,6 +1,13 @@
+using BudgetPlan.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
 namespace BudgetPlan.Application.Common.Interfaces;
 
 public interface IBudgetPlanDbContext
 {
+    DbSet<TransactionCategory> TransactionCategories { get; set; }
+    DbSet<TransactionDetails> TransactionDetails { get; set; }
+    DbSet<BudgetPlanDetails> BudgetPlanDetails { get; set; }
+    
     Task<int> SaveChangesAsync(CancellationToken cancellationToken);
 }
