2bb4ae14eecf0ffcbd28683fb6e8c2852e51c8f2
Adam Ludwiczak <adam.ludwiczak@keikostudio.com>
2023-07-31 07:58:08 +0200

    Added Authorize to all controllers

--- BudgetPlan.Api/Controllers/BaseController.cs
+++ BudgetPlan.Api/Controllers/BaseController.cs
@@ -1,9 +1,11 @@
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BudgetPlan.Api.Controllers;
 
 [ApiController]
+[Authorize]
 public class BaseController : ControllerBase
 {
     private IMediator _mediator;
