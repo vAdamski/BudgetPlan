2546f21a4099dfa51a589d34cae6a7cc499b3487
Adam Ludwiczak <adam.ludwiczak@keikostudio.com>
2023-08-05 18:24:59 +0200

    Clean project

--- BudgetPlan.Shared/ViewModels/BudgetPlanDetailsViewModel.cs
+++
@@ -1,8 +0,0 @@
-using BudgetPlan.Shared.Dtos;
-
-namespace BudgetPlan.Shared.ViewModels;
-
-public class BudgetPlanDetailsViewModel
-{
-    public List<OverTransactionCategoryWithDetailsDto> OverTransactionCategoryWithDetailsDtos { get; set; } = new();
-}
