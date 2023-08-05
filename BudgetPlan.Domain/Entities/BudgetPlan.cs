1a3578eaa413adccd03f6c07cb20ed1b0bd39a79
Adam Ludwiczak <adam.ludwiczak@keikostudio.com>
2023-08-05 14:22:01 +0200

    Added constructor to BudgetPlan

--- BudgetPlan.Domain/Entities/BudgetPlan.cs
+++ BudgetPlan.Domain/Entities/BudgetPlan.cs
@@ -4,7 +4,14 @@ namespace BudgetPlan.Domain.Entities;
 
 public class BudgetPlan : AuditableEntity
 {
+    public BudgetPlan(int year, int month)
+    {
+        DateFrom = new DateTime(year, month, 1);
+        DateTo = DateFrom.AddMonths(1).AddDays(-1);
+    }
+    
     public DateTime DateFrom { get; set; }
     public DateTime DateTo { get; set; }
+    
     public List<BudgetPlanDetails> BudgetPlanDetailsList { get; set; } = new();
 }
