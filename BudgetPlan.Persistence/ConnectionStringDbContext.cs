9aa74d2f797b979793eeef88942d0feb6cbcbae6
Adam Ludwiczak <adam.ludwiczak@keikostudio.com>
2023-09-14 19:51:36 +0200

    Database ConnectionString update

--- BudgetPlan.Persistence/ConnectionStringDbContext.cs
+++ BudgetPlan.Persistence/ConnectionStringDbContext.cs
@@ -4,8 +4,7 @@ public static class ConnectionStringDbContext
 {
     public static string GetConnectionString()
     {
-        var connectionString =
-            "Server=tcp:budgetplan.database.windows.net,1433;Initial Catalog=budgetPlan;Persist Security Info=False;User ID=budgetplan_admin;Password=U9VVTmZaWhxsqpvX7ptV;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        var connectionString = "Server=localhost,1433;Initial Catalog=BudgetPlanDatabase;Persist Security Info=False;User ID=sa;Password=Pass1234$;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;";
         return connectionString;
     }
 }
