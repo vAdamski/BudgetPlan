8ab7ea6165a146a699937778e7be58666268b1b9
Adam Ludwiczak <adam.ludwiczak@keikostudio.com>
2023-09-16 00:33:59 +0200

    Prepared first full unit test

--- ApplicationUnitTests/BudgetPlans/Commands/CreateBudgetPlan/CreateBudgetPlanCommandHandlerTests.cs
+++ ApplicationUnitTests/BudgetPlans/Commands/CreateBudgetPlan/CreateBudgetPlanCommandHandlerTests.cs
@@ -19,23 +19,60 @@ public class CreateBudgetPlanCommandHandlerTests : CommandTestBase
     public async Task Handle_GivenValidCommand_ShouldCreateBudgetPlanWithAllCategories()
     {
         // Arrange
-        var command = new CreateBudgetPlanCommand()
+        var todayDate = DateTime.Today;
+        var dateTimeFirstDayOfCurrentMonth = GetDateWithFirstDayOfCurrentMonth();
+        var dateTimeLastDayOfCurrentMonth = GetDateWithLastDayOfCurrentMonth();
+        
+        var command = new CreateBudgetPlanCommand
         {
-            Date = new DateTime(2023, 8, 1)
+            Date = todayDate
         };
+
+        var nextBudgetPlanId = GetNextExpectedBudgetPlanId();
 
         // Act
         var result = await _handler.Handle(command, CancellationToken.None);
 
         // Assert
-        Assert.Equal(1, result);
+        Assert.Equal(nextBudgetPlanId, result);
 
-        var underCategoriesCount = _context.TransactionCategories
-            .Count(x => x.StatusId == 1 && x.OverTransactionCategoryId != null);
+        var underCategoriesCount = GetCountOfUnderCategoriesForCurrentUser();
 
-        var budgetPlan = _context.BudgetPlans.Where(x => x.CreatedBy == _currentUserService.Email)
-            .Include(x => x.BudgetPlanDetailsList).FirstOrDefault();
-        
+        var budgetPlan = await _context.BudgetPlans.Where(x => x.CreatedBy == _currentUserService.Email && x.Id == result)
+            .Include(x => x.BudgetPlanDetailsList).FirstOrDefaultAsync();
+
+        budgetPlan.ShouldNotBeNull();
+        budgetPlan.DateFrom.ShouldBe(dateTimeFirstDayOfCurrentMonth);
+        budgetPlan.DateTo.ShouldBe(dateTimeLastDayOfCurrentMonth);
         budgetPlan.BudgetPlanDetailsList.Count.ShouldBe(underCategoriesCount);
+    }
+    
+    private DateTime GetDateWithFirstDayOfCurrentMonth()
+    {
+        var date = new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1);
+        return date;
+    }
+    
+    private DateTime GetDateWithLastDayOfCurrentMonth()
+    {
+        var date = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.DaysInMonth(DateTime.Today.Year, DateTime.Today.Month));
+        return date;
+    }
+
+    private int GetCountOfUnderCategoriesForCurrentUser()
+    {
+        var underCategoriesCount = _context.TransactionCategories
+            .Count(x => x.StatusId == 1 && 
+                        x.OverTransactionCategoryId != null && 
+                        x.CreatedBy == _currentUserService.Email);
+
+        return underCategoriesCount;
+    }
+    
+    private int GetNextExpectedBudgetPlanId()
+    {
+        var lastBudgetPlanId = _context.BudgetPlans.Max(x => x.Id);
+        var nextBudgetPlanId = lastBudgetPlanId + 1;
+        return nextBudgetPlanId;
     }
 }
