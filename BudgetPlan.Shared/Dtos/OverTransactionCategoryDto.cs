ecb32b5be809660c63b2c5a7f3f8e5e55e5a3601
Adam Ludwiczak <adam.ludwiczak@keikostudio.com>
2023-10-07 22:42:26 +0200

    Added new constructor to OverTransactionCategoryDto

--- BudgetPlan.Shared/Dtos/OverTransactionCategoryDto.cs
+++ BudgetPlan.Shared/Dtos/OverTransactionCategoryDto.cs
@@ -1,9 +1,20 @@
-using BudgetPlan.Shared.Enums;
+using BudgetPlan.Domain.Entities;
 
 namespace BudgetPlan.Shared.Dtos;
 
 public class OverTransactionCategoryDto
 {
+    public OverTransactionCategoryDto()
+    {
+        
+    }
+    
+    public OverTransactionCategoryDto(TransactionCategory transactionCategory)
+    {
+        Id = transactionCategory.Id;
+        TransactionCategoryName = transactionCategory.TransactionCategoryName;
+    }
+    
     public Guid Id { get; set; }
     public string TransactionCategoryName { get; set; }
     public List<TransactionCategoryDto> TransactionCategoryDtos { get; set; } = new();
