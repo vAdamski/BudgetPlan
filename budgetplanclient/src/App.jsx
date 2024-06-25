4f6dd395a399988752b5d2aacee07effdf27677d
Adam Ludwiczak <64928495+vAdamski@users.noreply.github.com>
2024-06-25 14:48:10 +0200

    Remove Bootstrap dependency from the project
    The Bootstrap and associated dependencies have been removed from the project as they are not needed. This includes changes

--- budgetplanclient/src/App.jsx
+++ budgetplanclient/src/App.jsx
@@ -1,5 +1,4 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import {AuthProvider} from './services/authProvider.jsx';
 import LoginCallback from './services/loginCallback.jsx';
