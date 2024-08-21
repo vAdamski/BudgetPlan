ae72b5f6f6e2ba61d5eb008846f79f7feeeb8a49
Adam Ludwiczak <64928495+vAdamski@users.noreply.github.com>
2024-08-22 00:54:24 +0200

    Add error handling to IncomingRequestLoggerMiddleware
    This change wraps the logging process in a try-catch block to handle potential exceptions. It ensures that any error encountered while logging the incoming request is caught and logged appropriately without disrupting the request processing pipeline.

--- BudgetPlan.Api/Middlewares/IncomingRequestLoggerMiddleware.cs
+++ BudgetPlan.Api/Middlewares/IncomingRequestLoggerMiddleware.cs
@@ -4,22 +4,29 @@ public class IncomingRequestLoggerMiddleware(ILogger<IncomingRequestLoggerMiddle
 {
 	public Task InvokeAsync(HttpContext context, RequestDelegate next)
 	{
-		var request = context.Request;
-		var requestMethod = request.Method;
-		var requestPath = request.Path;
-		var requestQueryString = request.QueryString;
-		var requestBody = string.Empty;
-
-		if (request.ContentLength > 0)
+		try
 		{
-			request.EnableBuffering();
-			requestBody = new StreamReader(request.Body).ReadToEnd();
-			request.Body.Position = 0;
+			var request = context.Request;
+			var requestMethod = request.Method;
+			var requestPath = request.Path;
+			var requestQueryString = request.QueryString;
+			var requestBody = string.Empty;
+
+			if (request.ContentLength > 0)
+			{
+				request.EnableBuffering();
+				requestBody = new StreamReader(request.Body).ReadToEnd();
+				request.Body.Position = 0;
+			}
+
+			logger.LogDebug($"Incoming request: {requestMethod} {requestPath}{requestQueryString}");
+			logger.LogDebug($"Request body: {requestBody}");
 		}
-
-		logger.LogDebug($"Incoming request: {requestMethod} {requestPath}{requestQueryString}");
-		logger.LogDebug($"Request body: {requestBody}");
-
+		catch (Exception ex)
+		{
+			logger.LogError(ex, "An error occurred while logging incoming request");
+		}
+		
 		return next(context);
 	}
 }
